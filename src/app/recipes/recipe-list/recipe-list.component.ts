04348857a2792f909d89a31a3ca451bdbf2034f3
Saad Zafar <31594740+saadie123@users.noreply.github.com>
2018-04-06 19:46:11 +0500

    Added recipe and ingredient model and added sample content in the components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe("A Test Recipe","This is just a test recipe","https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg")
+  ];
   constructor() { }
 
   ngOnInit() {
