1c67718bb84e62ff32ecb4cb5e4bf2949e07b2fe
Saad Zafar <31594740+saadie123@users.noreply.github.com>
2018-04-07 14:26:40 +0500

    Fixed dropdown menu

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-
+  isMenuOpen = false;
   constructor() { }
 
   ngOnInit() {
